c8e4f3a1b81792f8b08dbb5c131b81c2aefb0929
rs <rs@rsmbp.local>
2019-02-24 16:47:59 +0700

    created the event model

--- event_website/event_website/models.py
+++ event_website/event_website/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db import models, transactions
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    start_date = models.DateField()
+    finish_date = models.DateField()
+    details = models.TextField(max_length=1000)
+    created_by = models.ForeignKey(User, related_name='created_event', on_delete=models.DO_NOTHING)
+    users_attending = models.ManyToManyField(User, related_name='users_attending')
+    active = models.BooleanField()
