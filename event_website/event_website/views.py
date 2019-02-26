3fb7b046aaa30a9bb72a0dc98db214c5e5038449
rraihansaputra <rraihansaputra@gmail.com>
2019-02-26 08:51:50 +0700

    added messages at home page and added disabled button for attended/created events

--- event_website/event_website/views.py
+++ event_website/event_website/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 from .models import Event
@@ -37,6 +38,7 @@ def event_create_view(request):
         event = form.save(commit=False)
         event.created_by = request.user
         event.save()
+        messages.info(request, "You have successfully created event {}".format(event.name))
         return redirect('home')
     return render(request,'create_event.html', {'form': form})
     #TODO require login
@@ -52,9 +54,11 @@ def event_attend_view(request):
             user = User.objects.get(pk=user_id)
             event.users_attending.add(user)
             event.save()
-            return redirect(home)
+            messages.info(request,"You have successfully attended {}".format(event.name))
+            return home(request)
         else:
-            return redirect(home)
+            messages.info(request, "You cannot attend events you have created.")
+            return home(request)
     #TODO handle these cases:
     #   how to handled attended event (handle on template?)
     #TODO require login
